5761bd4d3f879971dd5be71f0bad34225c33e1ff
woodorl <wizardkwh@gmail.com>
2013-11-24 12:19:18 +0900

    Default Work
    Default Work

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+var scene;
+
+var init = function() {
+    scene = document.querySelector('#scene');
+};
+
+window.onload = function() {
+    init();
+};
+
+window.onmousemove = function(e) {
+    var dx = (window.innerWidth / 2 - e.pageX) * 0.05;
+    scene.style['-webkit-transform'] = 'rotateY(' + dx + 'deg)';
+};
